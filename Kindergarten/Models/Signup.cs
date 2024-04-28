a79114d064a4597e1d0eb3011b2e6f9fc1c44447
mhmddhwl469 <167224326+mhmddhwl469@users.noreply.github.com>
2024-04-29 00:05:49 +0300

    Add files via upload

---
+++ Kindergarten/Models/Signup.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace kinder.Models
+{
+    public class Signup
+    {
+        [Key]
+        public int? UserId { get; set; }
+        public string? FullName { get; set; }
+        public int? Age { get; set; }
+        public int? CardNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Phone { get; set; }
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? AnswerSecurty { get; set; }
+    }
+}
