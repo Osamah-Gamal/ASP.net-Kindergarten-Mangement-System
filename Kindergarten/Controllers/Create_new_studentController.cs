a3578fa67d4974d09362a1e40d1f843a61c03dca
alqbaty30 <157643942+alqbaty30@users.noreply.github.com>
2024-04-28 23:37:29 +0300

    Add files via upload

---
+++ Kindergarten/Controllers/Create_new_studentController.cs
@@ -0,0 +1,32 @@
+﻿using Kindergarten.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kindergarten.Controllers
+{
+    public class Create_new_studentController : Controller
+    {
+
+        public readonly Studentcontext _db;
+        public Create_new_studentController(Studentcontext db)
+        {
+            _db = db;
+        }
+       
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Student stu)
+        {
+            _db.Items.Add(stu);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
