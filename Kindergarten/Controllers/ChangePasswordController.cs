14882b210540233dce17efffc2f0f3a59b132359
Osamah Gamal Mohammed Esmail <95630350+Osamah-Gamal@users.noreply.github.com>
2024-04-28 23:23:35 +0300

    Add files via upload

---
+++ Kindergarten/Controllers/ChangePasswordController.cs
@@ -0,0 +1,69 @@
+﻿using Kindergarten.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kindergarten.Controllers
+{
+    public class ChangePasswordController : Controller
+    {
+
+        //-------------
+        private readonly AccountContext _DB_Uesr;
+        public ChangePasswordController(AccountContext DB_Uesr)
+        {
+            _DB_Uesr = DB_Uesr;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(Signup n_user)
+        {
+            string uname = TempData["Username"].ToString();
+
+            string username = uname;
+
+            string confirmpassword = n_user.Password;
+
+
+            if (string.IsNullOrWhiteSpace(username) && string.IsNullOrWhiteSpace(confirmpassword))
+            {
+                return View();
+            }
+
+
+            var user = _DB_Uesr.Users.FirstOrDefault(u => u.Username == n_user.Username);
+
+            if (user != null)
+            {
+                if (n_user.Password == confirmpassword)
+                {
+                    user.Password = n_user.Password;
+                    _DB_Uesr.SaveChanges();
+
+
+                    /*---------- Go To Another Page by(Redirect)--------*/
+                    return View();
+                }
+                else
+                {
+
+                    ModelState.AddModelError("ConfirmPassword", "Password is mach...!");
+
+                }
+
+            }
+
+
+            TempData["Username"] = username;
+
+            return View();
+
+
+        }
+    }
+}
---
+++ Kindergarten/Controllers/Forget_my_passwordController.cs
@@ -0,0 +1,63 @@
+﻿using Kindergarten.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kindergarten.Controllers
+{
+    public class Forget_my_passwordController : Controller
+    {
+
+
+        //-------------
+        private readonly AccountContext _DB_Uesr;
+        public Forget_my_passwordController(AccountContext DB_Uesr)
+        {
+            _DB_Uesr = DB_Uesr;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //------------
+        public ActionResult Index(Signup n_user)
+        {
+
+            // قراءة القيم المدخلة من الصفحة
+            string username = Request.Form["Username"];
+            string password = Request.Form["Password"];
+
+            // التحقق من وجود المستخدم في قاعدة البيانات
+            bool usernameExists = _DB_Uesr.Users.Any(u => u.Username == username);
+            bool passwordExists = _DB_Uesr.Users.Any(u => u.Password == password);
+
+            if (usernameExists && passwordExists)
+            {
+
+
+                // تخزين اسم المستخدم في TempData
+                TempData["Username"] = username;
+
+
+                return RedirectToAction("Index", "Change_Password");
+
+            }
+            else
+            {
+                if (!usernameExists)
+                {
+                    ModelState.AddModelError("Username", "This username not correct...!");
+                }
+                if (!passwordExists)
+                {
+                    ModelState.AddModelError("Password", "This password not correct...!");
+                }
+                return View();
+            }
+
+
+        }
+    }
+}
