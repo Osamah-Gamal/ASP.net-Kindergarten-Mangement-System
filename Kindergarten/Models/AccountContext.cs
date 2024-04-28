e1decbe1c07add7c26a19d53338b9accff90fb6a
Osamah Gamal Mohammed Esmail <95630350+Osamah-Gamal@users.noreply.github.com>
2024-04-28 23:32:03 +0300

    Add files via upload

---
+++ Kindergarten/Models/AccountContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Models
+{
+    public class AccountContext:DbContext
+    {
+
+        public AccountContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Signup> Users { get; set; }
+    }
+}
