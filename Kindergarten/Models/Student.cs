c57936aef1ba3a8bf734f484790ad9a60c63c5a2
alqbaty30 <157643942+alqbaty30@users.noreply.github.com>
2024-04-28 23:40:16 +0300

    Add files via upload

---
+++ Kindergarten/Models/Student.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Kindergarten.Models
+{
+    public class Student
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+
+
+        public string Name { get; set; }
+
+
+        public int Age { get; set; }
+
+        public DateTime Birth { get; set; }
+
+      
+        public int Card { get; set; }
+
+        public string Gender { get; set; }
+
+
+        public int Level { get; set; }
+
+        public string Address { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ Kindergarten/Models/Studentcontext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kindergarten.Models
+{
+    public class Studentcontext:DbContext
+    {
+        public Studentcontext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Student> Items { get; set; }
+
+    }
+}
