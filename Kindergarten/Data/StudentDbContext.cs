cb1882281a8bc6de6be7d4bde5f3684b03fe3b36
MAHDi <156613568+mahdiamthib2024@users.noreply.github.com>
2024-04-27 20:26:27 -0700

    Mahdi files

---
+++ Kindergarten/Data/StudentDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Kid.Models.DBS;
+
+namespace Kid.Data
+{
+    public class StudentDbContext:DbContext
+    {
+        public StudentDbContext(DbContextOptions option) : base(option)
+        {
+
+        }
+        public DbSet<Student> students { get; set; }
+    }
+}
